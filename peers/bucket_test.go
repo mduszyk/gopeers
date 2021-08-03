80627066c3df8432b9ba947ca9526e8ddb217918
Michał Duszyk <michal@duszyk.com>
2021-08-03 12:07:42 +0200

    adding bucket tests

--- peers/bucket_test.go
+++ peers/bucket_test.go
@@ -1,8 +1,10 @@
 package peers
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
+	"time"
 )
 
 func TestInRange(t *testing.T) {
@@ -26,5 +28,95 @@ func TestInRange(t *testing.T) {
 	c := new(big.Int).Add(hi, big.NewInt(1))
 	if bucket.inRange(c) {
 		t.Errorf("value %d should not be in range\n", c)
+	}
+	d, err := RandomId()
+	if err != nil {
+		t.Errorf("failed generating random id: %v\n", err)
+	}
+	if !bucket.inRange(d) {
+		t.Errorf("random id %d should be in range\n", d)
+	}
+	e := Sha1Id([]byte("test123"))
+	if !bucket.inRange(e) {
+		t.Errorf("sha1 id %d should be in range\n", e)
+	}
+
+}
+
+func TestAdd(t *testing.T) {
+	lo := big.NewInt(0)
+	hi := maxId
+	bucket := NewBucket(20, lo, hi)
+	id := Sha1Id([]byte("test123"))
+	peer := Peer{id, nil, time.Now()}
+	if !bucket.add(peer) {
+		t.Errorf("bucket should add peer\n")
+	}
+	if !bucket.contains(id) {
+		t.Errorf("bucket should contain added peer\n")
+	}
+}
+
+func TestFull(t *testing.T) {
+	k := 20
+	lo := big.NewInt(0)
+	hi := maxId
+	bucket := NewBucket(k, lo, hi)
+	for i := 0; i < k; i++ {
+		id := Sha1Id([]byte(fmt.Sprintf("test%d", i)))
+		peer := Peer{id, nil, time.Now()}
+		if !bucket.add(peer) {
+			t.Errorf("bucket should add peer %d\n", i)
+		}
+	}
+	if !bucket.isFull() {
+		t.Errorf("bucket should be full\n")
+	}
+	id := Sha1Id([]byte("test123"))
+	peer := Peer{id, nil, time.Now()}
+	if bucket.add(peer) {
+		t.Errorf("bucket should not add peer\n")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	lo := big.NewInt(0)
+	hi := maxId
+	bucket := NewBucket(20, lo, hi)
+	for i := 0; i < 10; i++ {
+		id := Sha1Id([]byte(fmt.Sprintf("test%d", i)))
+		peer := Peer{id, nil, time.Now()}
+		if !bucket.add(peer) {
+			t.Errorf("bucket should add peer %d\n", i)
+		}
+	}
+	id := Sha1Id([]byte("test5"))
+	if !bucket.contains(id) {
+		t.Errorf("bucket should contain peer\n")
+	}
+	if !bucket.remove(id) {
+		t.Errorf("bucket should remove peer\n")
+	}
+	if bucket.contains(id) {
+		t.Errorf("bucket should not contain peer\n")
+	}
+
+}
+
+func TestFind(t *testing.T) {
+	lo := big.NewInt(0)
+	hi := maxId
+	bucket := NewBucket(20, lo, hi)
+	for i := 0; i < 10; i++ {
+		id := Sha1Id([]byte(fmt.Sprintf("test%d", i)))
+		peer := Peer{id, nil, time.Now()}
+		if !bucket.add(peer) {
+			t.Errorf("bucket should add peer %d\n", i)
+		}
+	}
+	id := Sha1Id([]byte("test5"))
+	i := bucket.find(id)
+	if i != 5 {
+		t.Errorf("bucket should find peer, position: %d\n", i)
 	}
 }
