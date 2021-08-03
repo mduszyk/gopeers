0e61821fa9b9ee6657e2bf89533e7c3deeea6629
Michał Duszyk <michal@duszyk.com>
2021-08-03 10:32:10 +0200

    improving test messages

--- peers/bucket_test.go
+++ peers/bucket_test.go
@@ -10,21 +10,21 @@ func TestInRange(t *testing.T) {
 	hi := maxId
 	bucket := NewBucket(20, lo, hi)
 	if !bucket.inRange(lo) {
-		t.Errorf("low not considered in range\n")
+		t.Errorf("low should be in range\n")
 	}
 	if bucket.inRange(hi) {
-		t.Errorf("hi considered in range\n")
+		t.Errorf("hi should not be in range\n")
 	}
 	a := new(big.Int).Sub(lo, big.NewInt(1))
 	if bucket.inRange(a) {
-		t.Errorf("value: %d should not be in range\n", a)
+		t.Errorf("value %d should not be in range\n", a)
 	}
 	b := new(big.Int).Sub(hi, big.NewInt(1))
 	if !bucket.inRange(b) {
-		t.Errorf("value: %d should be in range\n", b)
+		t.Errorf("value %d should be in range\n", b)
 	}
 	c := new(big.Int).Add(hi, big.NewInt(1))
 	if bucket.inRange(c) {
-		t.Errorf("value: %d should not be in range\n", c)
+		t.Errorf("value %d should not be in range\n", c)
 	}
 }
