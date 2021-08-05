b3264f96be39c10954130a12fb103a5343d97679
Michał Duszyk <michal@duszyk.com>
2021-08-05 10:57:33 +0200

    adding test for bucket list split

--- peers/bucketlist_test.go
+++ peers/bucketlist_test.go
@@ -31,3 +31,31 @@ func TestAddFind(t *testing.T) {
 		}
 	}
 }
+
+func TestBucketListSplit(t *testing.T) {
+	nodeId := big.NewInt(0)
+	nodePeer := Peer{nodeId, nil, time.Now()}
+	k := 20
+	splitLevelB := 5
+	bucketList := NewBucketList(k, splitLevelB, nodePeer)
+	for i := 0; i < k+1; i++ {
+		id := Sha1Id([]byte(fmt.Sprintf("test%d", i)))
+		peer := Peer{id, nil, time.Now()}
+		if !bucketList.add(peer) {
+			t.Errorf("bucket should add peer %d\n", i)
+		}
+	}
+	if len(bucketList.buckets) != 2 {
+		t.Errorf("there should be 2 buckets\n")
+	}
+	for i := 0; i < k+1; i++ {
+		id := Sha1Id([]byte(fmt.Sprintf("test%d", i)))
+		j, b := bucketList.find(id)
+		if j < 0 {
+			t.Errorf("bucket list should find bucket containing given id\n")
+		}
+		if !b.contains(id) {
+			t.Errorf("bucket should contain given id\n")
+		}
+	}
+}
