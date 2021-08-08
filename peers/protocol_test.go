9596efb7e5f75f44addbbd181640458c07d7bd19
Michał Duszyk <michal@duszyk.com>
2021-08-08 11:52:05 +0200

    adding method call protocol test

--- peers/protocol_test.go
+++ peers/protocol_test.go
@@ -44,3 +44,38 @@ func TestUdpProtocol(t *testing.T) {
 		t.Errorf("id of node 2 not added to bucket in node 1\n")
 	}
 }
+
+func TestMethodCallProtocol(t *testing.T) {
+	node1Id, err := RandomId()
+	if err != nil {
+		t.Errorf("failed generating random id: %v\n", err)
+	}
+	node1Peer := Peer{Id: node1Id, LastSeen: time.Now()}
+	node1BucketList := NewBucketList(20, 5, node1Peer)
+	p2pNode1 := NewP2pNode(node1Id, node1BucketList)
+	node1Peer.Protocol = p2pNode1
+
+	node2Id, err := RandomId()
+	if err != nil {
+		t.Errorf("failed generating random id: %v\n", err)
+	}
+	node2Peer := Peer{Id: node2Id, LastSeen: time.Now()}
+	node2BucketList := NewBucketList(20, 5, node2Peer)
+	p2pNode2 := NewP2pNode(node2Id, node2BucketList)
+	node2Peer.Protocol = p2pNode2
+
+	randomId, err := RandomId()
+	if err != nil {
+		t.Errorf("failed generating random Id: %v\n", err)
+	}
+	echoId, err := p2pNode1.Ping(node2Peer, randomId)
+	if err != nil {
+		t.Errorf("failed pinging: %v\n", err)
+	}
+	if echoId.Cmp(randomId) != 0 {
+		t.Errorf("ping returned invalid Id\n")
+	}
+	if i, _ := p2pNode1.buckets.find(node2Id); i < 0 {
+		t.Errorf("id of node 2 not added to bucket in node 1\n")
+	}
+}
