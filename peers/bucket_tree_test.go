f7a512935d3e14ee812483e45fb8ee43e6d599e5
Michał Duszyk <michal@duszyk.com>
2021-08-11 17:30:05 +0200

    improving tree closest test

--- peers/bucket_tree_test.go
+++ peers/bucket_tree_test.go
@@ -1,6 +1,8 @@
 package peers
 
 import (
+	"math/big"
+	"sort"
 	"testing"
 )
 
@@ -37,11 +39,13 @@ func TestTreeClosest(t *testing.T) {
 	tree.root.left.split()
 	tree.root.right.split()
 	tree.root.right.right.split()
+	allPeers := make([]*Peer, 0, 100)
 	setBits1 := []uint{IdBits - 1, IdBits - 2, IdBits - 3}
 	bucket1 := tree.root.right.right.right.bucket
 	for i := 0; i < 10; i++ {
 		setBits1 = append(setBits1, uint(i))
 		peer := &Peer{Id: intBits(setBits1)}
+		allPeers = append(allPeers, peer)
 		if !bucket1.add(peer) {
 			t.Errorf("failed adding to bucket\n")
 		}
@@ -51,6 +55,7 @@ func TestTreeClosest(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		setBits2 = append(setBits2, uint(i))
 		peer := &Peer{Id: intBits(setBits2)}
+		allPeers = append(allPeers, peer)
 		if !bucket2.add(peer) {
 			t.Errorf("failed adding to bucket\n")
 		}
@@ -60,7 +65,18 @@ func TestTreeClosest(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		setBits3 = append(setBits3, uint(i))
 		peer := &Peer{Id: intBits(setBits3)}
+		allPeers = append(allPeers, peer)
 		if !bucket3.add(peer) {
+			t.Errorf("failed adding to bucket\n")
+		}
+	}
+	setBits4 := []uint{32}
+	bucket4 := tree.root.left.left.bucket
+	for i := 0; i < 5; i++ {
+		setBits4 = append(setBits4, uint(i))
+		peer := &Peer{Id: intBits(setBits4)}
+		allPeers = append(allPeers, peer)
+		if !bucket4.add(peer) {
 			t.Errorf("failed adding to bucket\n")
 		}
 	}
@@ -70,5 +86,16 @@ func TestTreeClosest(t *testing.T) {
 	peers := tree.closest(peer.Id, n)
 	if len(peers) != n {
 		t.Errorf("returned wrong number of close peers: %d\n", len(peers))
+	}
+	sort.Slice(allPeers, func(i, j int) bool {
+		di := new(big.Int).Xor(peer.Id, allPeers[i].Id)
+		dj := new(big.Int).Xor(peer.Id, allPeers[j].Id)
+		return di.Cmp(dj) == -1
+	})
+	expected := allPeers[:n]
+	for i := 0; i < n; i++ {
+		if expected[i].Id.Cmp(peers[i].Id) != 0 {
+			t.Errorf("ids don't match\n")
+		}
 	}
 }
